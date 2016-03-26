b3013a7b7c6e3604e760ad9d9b6b7c733de42219
jhonjairoroa87 <jhonjairoroa87@gmail.com>
2016-03-25 19:25:38 -0500

    added support to identify multiple groups that match the sum criteria

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 // print variable name and value
 void print_variable(string var_name, int var_value){
-    cout << "value of " << var_name << " : " << var_value << endl;
+    cout << var_name << " : " << var_value << endl;
 }
 
 // print elements contained in an array
@@ -116,41 +116,45 @@ vector <int*> get_complete_indexes_vectors(int array_length, int group_length){
     return final_indexes_vector;
 }
 
-// validate if the group elements match the sum criteria
+// create the group combinations and validate if the group elements match the sum criteria
 // params:
 //  - init_array: array to be iterated and its elements will be summed. Ie, [1,2,3,4,5,6,7,8,9,10]
 //  - array_length: Length of the array that contains the elements to be grouped. Ie 10
-//  - group_length: Length of the groups to be created. Ie 3
 //  - group_sum: Value the valid group elements should be equal. Ie 9
-// return vector that contains the group. Ie <[1,2,6], [2,3,4]>
-vector <int*> iterate_main_array(int *init_array, int array_length, int group_length, int group_sum){
-    vector <int*> result_vector;
-    vector <int*> indexes_vector = get_complete_indexes_vectors(array_length, group_length);
-    // iterate the indexes to cerate the group and validate it sum value
-    for (std::vector<int*>::iterator it = indexes_vector.begin() ; it != indexes_vector.end(); ++it){
-        int* my_index_array = *it;
-        int* values_group = create_array_group(init_array, group_length, my_index_array);
-        // validate if the elements of the group sums the expected value
-        if (sum_array(values_group, group_length) == group_sum){
-            result_vector.push_back( values_group );
+// return vector that contains the group. Ie <[1,8], [2,7], [3,6] , [1,2,6], [2,3,4]>
+void iterate_main_array(int *init_array, int array_length, int group_sum){
+    // iterate thought all posible group lengths
+    for( int group_length = 2; group_length < array_length; group_length++){
+        vector <int*> result_vector;
+        vector <int*> indexes_vector = get_complete_indexes_vectors(array_length, group_length);
+        // iterate the indexes to cerate the group and validate it sum value
+        for (std::vector<int*>::iterator it = indexes_vector.begin() ; it != indexes_vector.end(); ++it){
+            int* my_index_array = *it;
+            int* values_group = create_array_group(init_array, group_length, my_index_array);
+            // validate if the elements of the group sums the expected value
+            if (sum_array(values_group, group_length) == group_sum){
+                result_vector.push_back( values_group );
+            }
         }
+        print_vector(result_vector, group_length);
     }
-    return result_vector;
 }
 
 int main() {
 
     int array_length = 20; // number of elements the array will contain
     int numbers [array_length] = { 8, 4, 3, 2, 5, 6, 7, 1, 9, 10, 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19,20 }; // numbers to be grouped
-    int group_length = 4;
-    int group_sum = 11;
+    int group_sum = 30;
 
     // sort from lower to greater value
     sort(numbers, numbers + array_length);
+
     print_array(numbers, array_length);
-    // create the groups and identify the ones that math the sum criteria
-    vector <int*> result_vector = iterate_main_array(numbers, array_length, group_length, group_sum);
-    print_vector(result_vector, group_length);
+    print_variable("group sum", group_sum);
+
+    // create the groups and identify the ones that match the sum criteria
+    iterate_main_array(numbers, array_length, group_sum);
+
 
     return 0;
 }
